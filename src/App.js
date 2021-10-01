8104c7e4fc2c0df6721133d2b2b2db416d1fec09
NGUYỄN TƯỜNG ĐĂNG <16520170@ms.uit.edu.vn>
2021-10-01 15:37:00 +0700

    Set up sass

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 
+import HomePage from "./homepage";
+
 function App() {
-    return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
