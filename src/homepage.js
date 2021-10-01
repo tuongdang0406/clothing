795ac202e08f377985c511451393bcc035bcaef9
NGUYỄN TƯỜNG ĐĂNG <16520170@ms.uit.edu.vn>
2021-10-01 17:08:54 +0700

    Set up sass

--- src/homepage.js
+++ src/homepage.js
@@ -6,7 +6,7 @@ const HomePage = () => (
     <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
-          <h1 className="title">HATS</h1>
+          <h1 className="title">HAT</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
@@ -14,7 +14,7 @@ const HomePage = () => (
     <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
-          <h1 className="title">JACKETS</h1>
+          <h1 className="title">JACKET</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
