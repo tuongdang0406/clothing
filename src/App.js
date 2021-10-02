a61a97e514edf6c06aa1cb30116d722e9f8d6729
NGUYỄN TƯỜNG ĐĂNG <16520170@ms.uit.edu.vn>
2021-10-02 23:53:41 +0700

    Test react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
+import { Route, Switch } from "react-router";
 
 import HomePage from "./pages/homepage/homepage";
 
+const HatPage = () => (
+  <div>
+    <h1>HAT</h1>
+  </div>
+);
+
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hat" component={HatPage} />
+      </Switch>
     </div>
   );
 }
