44fa5fe31d9b677631651841fe34a4304c5550a6
NGUYỄN TƯỜNG ĐĂNG <16520170@ms.uit.edu.vn>
2021-10-05 16:11:30 +0700

    Added sign up form

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,11 +1,13 @@
 import React from "react";
 import SignIn from "../../components/signin/signin";
+import SignUp from "../../components/signup/signup";
 
 import "./login.scss";
 
 const LoginPage = () => (
   <div className="login">
     <SignIn />
+    <SignUp />
   </div>
 );
 
