c59991f7d8028145521bf340447d646f184dc05c
NGUYỄN TƯỜNG ĐĂNG <16520170@ms.uit.edu.vn>
2021-10-07 09:48:32 +0700

    Modified sign in state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route, Switch } from "react-router";
+import { Route, Switch, Redirect } from "react-router";
 import { connect } from "react-redux";
 
 import HomePage from "./pages/homepage/homepage";
@@ -46,15 +46,25 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={LoginPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <LoginPage />
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
