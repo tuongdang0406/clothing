e9956136c534c791c8e55f860f9709a89329a31e
NGUYỄN TƯỜNG ĐĂNG <16520170@ms.uit.edu.vn>
2021-10-09 17:27:21 +0700

    fixing redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
-import persistStore from "redux-persist/es/persistStore";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
-const middleware = [];
+const middlewares = [logger];
 
-if (process.env.NODE_ENV !== "production") {
-  middleware.push(logger);
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middleware));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
